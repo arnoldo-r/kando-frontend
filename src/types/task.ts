fe567298cea5537a61ad8144bd91b564c428fcdc
Arnoldo <arnoldus.rf@gmail.com>
2025-05-10 19:10:32 -0400

    feat: add types and enum

---
+++ src/types/task.ts
@@ -0,0 +1,24 @@
+import { TaskStatus } from "../enums/task-status.enum";
+
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: TaskStatus;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface CreateTaskDto {
+  title: string;
+  description: string;
+  status?: TaskStatus;
+}
+
+export interface UpdateTaskDto {
+  title?: string;
+  description?: string;
+  status?: TaskStatus;
+}
+
+export type TaskStatusSummary = Record<TaskStatus, number>;
